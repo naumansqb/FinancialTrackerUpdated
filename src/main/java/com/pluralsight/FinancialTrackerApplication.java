0524cc16f0f3893169a547a91787df03bf435ad3
naumansqb <naumansqb@gmail.com>
2026-01-16 12:04:22 -0500

    DATABSE CONNECTIVITY CHECK

--- src/main/java/com/pluralsight/FinancialTrackerApplication.java
+++ src/main/java/com/pluralsight/FinancialTrackerApplication.java
@@ -18,6 +18,15 @@ public class FinancialTrackerApplication {
         // Initialize DAO
         TransactionDao transactionDao = new MySqlTransactionDao(dataSource);
 
+        // Test Connection
+        try (var conn = dataSource.getConnection()) {
+            System.out.println("Database connected successfully!");
+        } catch (Exception e) {
+            System.err.println("Could not connect to database. Please check your credentials.");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
         // Run UI
         UserInterface ui = new UserInterface(transactionDao);
         ui.start();
