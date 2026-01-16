16c2a07c0fc0e0637f238c7580cc22aebd7ab05b
naumansqb <naumansqb@gmail.com>
2026-01-16 11:11:47 -0500

    intial commit

--- src/main/java/com/pluralsight/FinancialTracker.java
+++ src/main/java/com/pluralsight/FinancialTracker.java
@@ -11,7 +11,7 @@ public class FinancialTracker {
     private static final ArrayList<Transaction> transactions = new ArrayList<>();
     private static final String FILE_NAME = "transactions.csv";
     private static final String DATE_PATTERN = "yyyy-MM-dd";
-    private static final String TIME_PATTERN = "HH:mm:ss";
+    private static final String TIME_PATTERN = "HH:mm";
     private static final String DATETIME_PATTERN = DATE_PATTERN + " " + TIME_PATTERN;
 
     private static final DateTimeFormatter DATE_FMT = DateTimeFormatter.ofPattern(DATE_PATTERN);
